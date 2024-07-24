45e82add4c1755a84370b3e8d8a9426cd9cbdae3
evanalifian <evanalifian@gmal.com>
2024-07-24 19:43:31 +0700

    working with file system to create, update, and delete markdown note

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,22 +1,36 @@
-import { __dirname } from "../app.js";
+import { __dirname, notesDir } from "../app.js";
 import fs from "fs";
 
-export const loadJSONFile = () => {
+export const loadJSONFile = (md) => {
   return JSON.parse(fs.readFileSync(`${__dirname}/notes/notes.json`, "utf-8"));
 };
 
-export const writeJSONFile = (formData) => {
-  fs.writeFileSync(`${__dirname}/notes/notes.json`, JSON.stringify(formData));
+export const writeJSONFile = (data) => {
+  fs.writeFileSync(`${__dirname}/notes/notes.json`, JSON.stringify(data));
 };
 
-export const writeMarkdownFile = (formData) => {
+export const loadMarkdownFiles = (md) => {
+  return fs.readdirSync(notesDir).map((note) => {
+    const content = fs.readFileSync(`${notesDir}/${note}`, "utf-8");
+    const fileName = note.split(".md").join("");
+    const title = fileName.includes("important-")
+      ? fileName.split("important-").join("")
+      : fileName;
+    const is_important = fileName.includes("important-") ? true : false;
+    return { path: `${notesDir}${note}`, content: md.render(content), title, is_important };
+  });
+};
+
+export const writeMarkdownFile = (data) => {
   fs.writeFileSync(
-    `${__dirname}/notes/${formData.note_title}.md`,
-    formData.note_content
+    `${notesDir}${data.is_important ? "important-" : ""}${
+      data.title
+    }.md`,
+    data.content
   );
 };
 
-export const rewriteMarkdownFile = (formData) => {
-  fs.unlinkSync(`${__dirname}/notes/${formData.old_title}.md`);
-  writeMarkdownFile(formData);
+export const rewriteMarkdownFile = (data) => {
+  fs.unlinkSync(data.old_path);
+  writeMarkdownFile(data);
 };
